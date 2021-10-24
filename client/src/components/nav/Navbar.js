daca02e93b341452d5c939e379bb70b7154223e1
Jacob Hough <jacobhough@Jacobs-MacBook-Pro.local>
2021-10-23 18:43:41 -0600

    started on front end

---
+++ client/src/components/nav/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+import { NavContainer } from "./styledComponents";
+
+const Navbar = () => {
+  return <NavContainer />;
+};
+
+export default Navbar;
---
+++ client/src/components/nav/styledComponents.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+
+export const NavContainer = styled.div`
+  width: 100%;
+  height: 50px;
+  border: 1px solid red;
+`;
